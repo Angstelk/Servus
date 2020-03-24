49a198469848cde944f34330e4c84a320b868e3d
Angstelk <angtbutan@gmail.com>
2020-03-24 04:10:40 +0100

    mfcc script

--- log_spectro.py
+++ log_spectro.py
@@ -10,5 +10,35 @@ f, t, Sxx = signal.spectrogram(sig, sf, scaling='spectrum')
 plt.pcolormesh(t, f, np.log10(Sxx))
 plt.ylabel('f [Hz]')
 plt.xlabel('t [sec]')
-plt.show()
+plt.savefig("mamnanodze")
+#plt.show()
 
+file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)),'go_test.wav')
+fs, signal = wav.read(file_name)
+signal = signal[:,0]
+
+# Example of pre-emphasizing.
+signal_preemphasized = speechpy.processing.preemphasis(signal, cof=0.98)
+
+# Example of staching frames
+frames = speechpy.processing.stack_frames(signal, sampling_frequency=fs, frame_length=0.020, frame_stride=0.01, filter=lambda x: np.ones((x,)),
+         zero_padding=True)
+
+# Example of extracting power spectrum
+power_spectrum = speechpy.processing.power_spectrum(frames, fft_points=512)
+print('power spectrum shape=', power_spectrum.shape)
+
+############# Extract MFCC features #############
+mfcc = speechpy.feature.mfcc(signal, sampling_frequency=fs, frame_length=0.020, frame_stride=0.01,
+             num_filters=40, fft_length=512, low_frequency=0, high_frequency=None)
+mfcc_cmvn = speechpy.processing.cmvnw(mfcc,win_size=301,variance_normalization=True)
+print('mfcc(mean + variance normalized) feature shape=', mfcc_cmvn.shape)
+
+mfcc_feature_cube = speechpy.feature.extract_derivative_feature(mfcc)
+print('mfcc feature cube shape=', mfcc_feature_cube.shape)
+
+############# Extract logenergy features #############
+logenergy = speechpy.feature.lmfe(signal, sampling_frequency=fs, frame_length=0.020, frame_stride=0.01,
+             num_filters=40, fft_length=512, low_frequency=0, high_frequency=None)
+logenergy_feature_cube = speechpy.feature.extract_derivative_feature(logenergy)
+print('logenergy features=', logenergy.shape)
